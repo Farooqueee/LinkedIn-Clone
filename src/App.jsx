9c482887a0711641ed9d355af249f2324e17f83f
Farooq Ahmed <hello.farooq@outlook.com>
2025-06-25 02:33:17 +0500

    Created the Input and Icon on Feed section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Feed from "./components/Feed";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
@@ -10,7 +11,7 @@ function App() {
         {/* body */}
         <div className="app__body">
           <Sidebar />
-          {/* feed */}
+          <Feed />
           {/* widgets */}
         </div>
       </div>
